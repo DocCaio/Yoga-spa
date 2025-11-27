e6d6500cd702d47bb595bff5075344d9b91c88c4
Caio Martins 😊 <108128119+DocCaio@users.noreply.github.com>
2025-11-27 08:10:19 -0300

    add: lib fonts

---
+++ src/app/components/Text/Text.vue
@@ -0,0 +1,31 @@
+
+<script setup lang="ts">
+
+import "./Text.css";
+
+</script>
+
+
+<template>
+    <h1>The dawn of a new era in health is here</h1>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+         Quo ducimus alias, rerum soluta laborum natus dolore voluptates, nemo explicabo illo reiciendis ad 
+        libero maxime ipsam vel magnam ullam quia doloremque.
+    </p>
+
+    <RouterLink to="/"
+    class="
+    btn
+    rounded-sm
+    text-white 
+    py-2 
+    px-4 
+    font-semibold   
+    transition 
+    duration-150
+  "
+>Join now</RouterLink>
+    
+
+</template>
--- src/app/modules/samples/pages/HomePage.vue
+++ src/app/modules/samples/pages/HomePage.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+import Text from '@/app/components/Text/Text.vue';
+
+</script>
+
 <template>
     <div>
         Home
+        <Text/>
     </div>
     
 </template>
